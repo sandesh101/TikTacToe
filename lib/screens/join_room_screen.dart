efe6a987e81553af4b7282a5055cd35efde6187a
Sandesh Rimal <rimal.sandesh11@gmail.com>
2022-05-16 10:27:40 +0545

    Created Join Room UI

--- lib/screens/join_room_screen.dart
+++ lib/screens/join_room_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:tictactoe_multiplayer/responsive/responsive.dart';
+import 'package:tictactoe_multiplayer/widgets/buttom.dart';
+import 'package:tictactoe_multiplayer/widgets/custom_text.dart';
+import 'package:tictactoe_multiplayer/widgets/custom_textfield.dart';
 
 class JoinRoom extends StatefulWidget {
   static String routeName = '/join-room';
@@ -9,8 +13,52 @@ class JoinRoom extends StatefulWidget {
 }
 
 class _JoinRoomState extends State<JoinRoom> {
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _gameIdController = TextEditingController();
+
+  @override
+  void dispose() {
+    super.dispose();
+    _nameController.dispose();
+    _gameIdController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    final size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Responsive(
+        child: Container(
+          margin: const EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const CustomText(
+                shadow: [
+                  Shadow(blurRadius: 10, color: Colors.blue),
+                ],
+                text: "Join Room",
+                fontSize: 70,
+              ),
+              SizedBox(
+                height: size.height * 0.08,
+              ),
+              CustomTextField(
+                  controller: _nameController, hintText: "Enter Your Nickname"),
+              SizedBox(
+                height: size.height * 0.04,
+              ),
+              CustomTextField(
+                  controller: _gameIdController, hintText: "Enter Game ID"),
+              SizedBox(
+                height: size.height * 0.04,
+              ),
+              Button(onTap: () {}, buttonText: "Join"),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
